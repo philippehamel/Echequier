15c8d80addaac17e379b4a0d8295cb02cc6276ee
Philippe Hamel <p.hamel@protonmail.com>
2017-12-14 16:30:34 -0500

    test
    test

--- sizing.py
+++ sizing.py
@@ -8,7 +8,38 @@ def resize_print(event):
 
 
 root.bind('<Configure>', resize_print)
-quitter = tk.Button(root, text="Quit", command=root.quit)
-quitter.grid()
 
-root.mainloop()
+quitter = tk.Button(root, text="Quit", command=root.quit)
+quitter.grid(row=1)
+
+canvas = tk.Canvas(root, height=500, width=700)
+canvas.grid(row=0)
+canvas.create_text(350, 250, text='\u265b')
+
+#root.mainloop()
+
+pieces = {
+            'a1': 'TB', 'b1': 'CB', 'c1': 'FB', 'd1': 'DB', 'e1': 'RB', 'f1': 'FB', 'g1': 'CB', 'h1': 'TB',
+            'a2': 'PB', 'b2': 'PB', 'c2': 'PB', 'd2': 'PB', 'e2': 'PB', 'f2': 'PB', 'g2': 'PB', 'h2': 'PB',
+            'a7': 'PN', 'b7': 'PN', 'c7': 'PN', 'd7': 'PN', 'e7': 'PN', 'f7': 'PN', 'g7': 'PN', 'h7': 'PN',
+            'a8': 'TN', 'b8': 'CN', 'c8': 'FN', 'd8': 'DN', 'e8': 'RN', 'f8': 'FN', 'g8': 'CN', 'h8': 'TN',
+        }
+
+caracteres_pieces = {'PB': '\u2659',
+                             'PN': '\u265f',
+                             'TB': '\u2656',
+                             'TN': '\u265c',
+                             'CB': '\u2658',
+                             'CN': '\u265e',
+                             'FB': '\u2657',
+                             'FN': '\u265d',
+                             'RB': '\u2654',
+                             'RN': '\u265a',
+                             'DB': '\u2655',
+                             'DN': '\u265b'
+                             }
+
+for position, pieces in pieces.items():
+    for i, code in caracteres_pieces.items():
+        if i == pieces:
+            print("Position: {} ; Code: {} ; Pieces: {}".format(position, pieces, code))
